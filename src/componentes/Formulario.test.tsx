cdbda91788bbd775adaac56ea01ca67ad4bd0273
Leonardo <leo-feitosa@live.com>
2023-09-05 07:27:44 -0300

    feat: Gerencia estados e rotas

---
+++ src/componentes/Formulario.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react"
+import { Formulario } from "./Formulario";
+
+//Jest
+test('quando  o input está vazio, novos participantes não podem ser adicionados', () => {
+  render(<Formulario/>)
+
+  // encontra no DOM o input
+  const input = screen.getByPlaceholderText('Insira os nomes do participantes');
+
+  // encontrar o botão
+  const botao = screen.getByRole('button');
+
+  // garantir que o input esteja  no documento
+  expect(input).toBeInTheDocument();
+
+  // garantir que o botão esteja desabilitado
+  expect(botao).toBeDisabled();
+})
---
+++ src/componentes/Formulario.tsx
@@ -0,0 +1,8 @@
+export const Formulario = () => {
+  return (
+    <form>
+      <input type="text" placeholder="Insira os nomes do participantes"/>
+      <button disabled={true}>Adicionar</button>
+    </form>
+  );
+}
